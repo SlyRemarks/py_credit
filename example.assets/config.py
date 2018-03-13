5ce4551975e8e195efe1773337db63152d039927
SlyRemarks <gwil.noble@openenergymonitor.org>
2018-03-13 13:24:11 +0000

    add examples

---
+++ example.assets/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+bigcommerce = {
+    
+    'client_id'    :  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'store_hash'   :  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'access_token' :  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'order_id'     :  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+}
+
+database = {
+    
+    'host'   : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'user'   : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'passwd' : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    'db'     : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+    
+}
